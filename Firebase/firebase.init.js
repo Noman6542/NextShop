cc3ee12859bc59efbf26b840f2a18f20464409e8
Noman6542 <mdaanoman6@gmail.com>
2025-11-24 21:30:08 +0600

    add all thing

--- Firebase/firebase.init.js
+++ Firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB-TVi2VUFaOUoyQRTr4-i11rU_Qr4tRaQ",
-  authDomain: "nextshop-21115.firebaseapp.com",
-  projectId: "nextshop-21115",
-  storageBucket: "nextshop-21115.firebasestorage.app",
-  messagingSenderId: "135637337486",
-  appId: "1:135637337486:web:dce496ce1c709b5ae810df"
+   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
